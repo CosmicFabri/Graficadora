8c322ca6ac7c0c5b27e954e7919f36cfe8a0563a
SantiagoGMG <al064489@uacam.mx>
2025-03-24 18:33:18 -0600

    feat:Se creo la clase graficador y sus funciones para crear el canvas y dibujar un punto

--- app/src/main/java/com/example/graficadora/MainActivity.kt
+++ app/src/main/java/com/example/graficadora/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.graficadora.ui.theme.GraficadoraTheme
@@ -20,10 +24,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             GraficadoraTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box()
+                    {
+                        Column {
+                            val graficadora = remember {Graficadora()}
+                            graficadora.Renderizar()
+
+                            Button(onClick = {
+                                graficadora.agregarPunto(100F,100F)
+                            }){
+                                Text("Punto de prueba")
+                            }
+                        }
+
+                    }
+
+
                 }
             }
         }
@@ -42,6 +58,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GraficadoraTheme {
-        Greeting("Android")
+        //Greeting("Android")
+        val graficadora = remember {Graficadora()}
+        graficadora.Renderizar()
+
     }
 }
